1064bf0422356d7d316d16bb1862a7a670a28560
96anastasia96 <ak96ak96@yandex.ru>
2023-07-20 16:03:09 +0300

    Updated

--- minigame1/models.py
+++ minigame1/models.py
@@ -7,19 +7,19 @@ WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
 PLAYER_SIZE = 50
 OBJECT_SIZE = 30
-PLAYER_COLOR = (0, 0, 255)
+PLAYER = 'images/robot.png'
 RIGHT_OBJECT_IMAGE = 'images/tomato.png'
 WRONG_OBJECT_IMAGE = 'images/unripe_tomato.png'
+WRONG2_OBJECT_IMAGE = 'images/black_tomato.png'
 BG_COLOR = (255, 255, 255)
 FPS = 60
 
 
 class FallingObject:
-    def __init__(self, x, y, image_path, right_object):
+    def __init__(self, x, y, image_path, earn_points):
         self.x = x
         self.y = y
-        # поле, указывающее на то, правильный объект или нет
-        self.right_object = right_object
+        self.earn_points = earn_points
         self.image = pygame.transform.scale(pygame.image.load(image_path), (OBJECT_SIZE, OBJECT_SIZE))
 
     def move(self):
@@ -33,18 +33,22 @@ class Player:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        # Загрузка изображения игрока
+        self.image = pygame.transform.scale(pygame.image.load(PLAYER), (PLAYER_SIZE, PLAYER_SIZE))
 
     def move(self, x):
         self.x += x
 
     def draw(self, window):
-        pygame.draw.rect(window, PLAYER_COLOR, pygame.Rect(self.x, self.y, PLAYER_SIZE, PLAYER_SIZE))
+        # Рисования изображения игрока
+        window.blit(self.image, (self.x, self.y))
 
 
 def start_the_game():
     player = Player(WINDOW_WIDTH // 2, WINDOW_HEIGHT - PLAYER_SIZE - 10)
     objects = []
     score = 1
+    vegetables_collected = 0
     run = True
     start_ticks = pygame.time.get_ticks()
     font = pygame.font.Font(None, 36)  # None для использования шрифта по умолчанию
@@ -64,47 +68,38 @@ def start_the_game():
         if keys[pygame.K_RIGHT] and player.x + 5 < WINDOW_WIDTH - PLAYER_SIZE:
             player.move(5)
 
+        rand_object = random.randint(1, 3)
         if random.randint(1, 20) == 1:
-            if random.randint(1, 2) == 1:
+            if rand_object == 1:
                 objects.append(FallingObject(random.randint(OBJECT_SIZE, WINDOW_WIDTH - OBJECT_SIZE), -OBJECT_SIZE,
-                                             RIGHT_OBJECT_IMAGE, True))
+                                             RIGHT_OBJECT_IMAGE, 4))
+            elif rand_object == 2:
+                objects.append(FallingObject(random.randint(OBJECT_SIZE, WINDOW_WIDTH - OBJECT_SIZE), -OBJECT_SIZE,
+                                             WRONG_OBJECT_IMAGE, 1))
             else:
                 objects.append(FallingObject(random.randint(OBJECT_SIZE, WINDOW_WIDTH - OBJECT_SIZE), -OBJECT_SIZE,
-                                             WRONG_OBJECT_IMAGE, False))
+                                             WRONG2_OBJECT_IMAGE, 2))
 
         for obj in objects:
             obj.move()
             if pygame.Rect(obj.x, obj.y, OBJECT_SIZE, OBJECT_SIZE).colliderect(
                     pygame.Rect(player.x, player.y, PLAYER_SIZE, PLAYER_SIZE)):
                 objects.remove(obj)
-                if obj.right_object:
-                    score += 1
-                else:
-                    score -= 1
+                score += obj.earn_points
+                vegetables_collected += 1
             elif obj.y > WINDOW_HEIGHT:
                 objects.remove(obj)
 
-        # обновление времени здесь
-        seconds = (pygame.time.get_ticks() - start_ticks) / 1000
-
-        if seconds >= 60 or score <= 0:
+        if vegetables_collected >= 20:
             run = False
-            if seconds >= 60:
-                final_score_text = font.render(f"Время вышло! Ваш счет: {score}", True, (10, 160, 10))
-            else:
-                final_score_text = font.render(f"Вы проиграли! Попробуйте еще раз.", True, (10, 160, 10))
+            final_score_text = font.render('Ваш счет: ' + str(score * 100), 1, (10, 10, 10))
+
 
         window.fill(BG_COLOR)
 
         # обновление текста счета
         text = font.render(f'Счет: {score}', 1, (100, 160, 10))
         window.blit(text, (10, 10))
-
-        # Время игры
-        total_game_time = 60
-        remaining_time = total_game_time - int(seconds)
-        game_time = font.render(f'Оставшееся время: {remaining_time}', 1, (100, 160, 10))
-        window.blit(game_time, (10, 50))
 
         if final_score_text is not None:
             window.blit(final_score_text, (WINDOW_WIDTH // 2 - final_score_text.get_width() // 2, WINDOW_HEIGHT // 2))
@@ -121,6 +116,10 @@ def start_the_game():
 
 pygame.init()
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+description = 'В этом мини-игре вам нужно управлять роботом и собрать 20 падающих овощей.'
+rules = 'За каждый собранный овощ вы получаете очки: зелёный - 2, переспелый - 1, спелый - 4. В конце раунда ваши очки умножаются на 100'
 menu = pygame_menu.Menu('Мини-Игра 1', WINDOW_WIDTH, WINDOW_HEIGHT, theme=pygame_menu.themes.THEME_BLUE)
+menu.add.label(description, max_char=-1, font_size=18)
+menu.add.label(rules, max_char=-1, font_size=18)
 menu.add.button('Играть', start_the_game)
 menu.mainloop(pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT)))
