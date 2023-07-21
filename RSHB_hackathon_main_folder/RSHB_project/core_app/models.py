da28b2b3ddf0b119173f41049d06e4a5636c4723
ivanbal93 <ivanbal93@gmail.com>
2023-07-21 20:44:28 +0400

    Подкорректировал модели,этот пул-реквест корректный

--- RSHB_hackathon_main_folder/RSHB_project/core_app/models.py
+++ RSHB_hackathon_main_folder/RSHB_project/core_app/models.py
@@ -1,39 +1,90 @@
 from django.db import models
 
 # Create your models here.
+# !!! Возможно, класс растений не нужен вовсе !!!
 
 
 class Bank(models.Model):  # Класс банка
     name = models.CharField(max_length=50)
-    bank_account = models.IntegerField()  # Денежный счёт
-
-
-man = 'm'
-woman = 'w'
-genders = [(man, 'Мужской'), (woman, 'Женский')]
-
-
-class Player(models.Model):  # Класс игрока
-    name = models.CharField(max_length=30)
-    gender = models.CharField(max_length=10, choices=genders, default=None)
-    bank_account = models.IntegerField()
-    issued_loan = models.BooleanField()  # Факт оформления кредита в банке
-    bank = models.ForeignKey(Bank, on_delete=models.CASCADE)
+    bank_account = models.IntegerField(default=1000)  # Денежный счёт
 
 
 class Shop(models.Model):  # Класс магазина
     name = models.CharField(max_length=50)
 
 
+class Plant(models.Model):  # Класс растений
+    name = models.CharField(max_length=50, unique=True)
+    value = models.IntegerField(default=1)
+
+
+class Game(models.Model):  # Класс мини-игр
+    name = models.CharField(max_length=50, unique=True)
+    completed = models.BooleanField(default=False)
+    result = models.IntegerField(default=0)
+
+    def game_complete(self):  # TODO
+        self.completed = True
+
+
 class Equipment(models.Model):  # Класс используемой игроком техники
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     price = models.IntegerField()
-    in_players_inventory = models.BooleanField()  # Факт наличия в инвентаре игрока
+    available = models.BooleanField(default=True if id == 1 else False)
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+
+    def become_available(self, game_id):  # Получение доступа к технике
+        game = Game.objects.get(id=game_id)
+        if game.id == self.id and game.completed:
+            self.available = True
+        self.save()
+
+    def add_to_inventory(self, player_id=int):  # Добавление в инвентарь
+        player = Player.objects.get(id=player_id)
+        player.inventory.add(self.id)
+        player.bank_accout -= self.price
+        player.save()
+        self.save()
+
+
+man = 'Мужской'
+woman = 'Женский'
+genders = [(man, 'Мужской'), (woman, 'Женский')]
+
+
+class Player(models.Model):  # Класс игрока
+    name = models.CharField(max_length=50, unique=True)
+    gender = models.CharField(max_length=10, choices=genders, default=None)
+    bank_account = models.IntegerField(default=100)  # Кошелёк игрока
+    credit = models.IntegerField(default=0)  # Размер кредита
+    inventory = tuple()
+    bank = models.ForeignKey(Bank, on_delete=models.CASCADE)
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    equipment = models.ManyToManyField(Equipment, through='PlayerEquipment')
+    game = models.ManyToManyField(Game, through='PlayerGame')
+
+    def get_a_credit(self, credit=int):  # Оформление кредита
+        self.credit = credit
+        self.bank_account += credit
+        self.save()
+
+    def return_a_credit(self):  # Возврат кредита
+        if self.bank_account >= self.credit:
+            self.bank_account -= self.credit
+            self.save()
+        else:
+            raise ValueError('На вашем счёте недостаточно средств для возврата кредита!')
+
+    def add_to_bank_account(self, total):  # Добавление выйгранных монет в кошелёк игрока
+        self.bank_account += total
+        self.save()
+
+
+class PlayerEquipment(models.Model):  # Промежуточный класс
+    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
 
 
-class Plant(models.Model):  # Класс растений
-    name = models.CharField(max_length=50)
-    ripeness = models.CharField(max_length=50)
-
+class PlayerGame(models.Model):  # Промежуточный класс
+    equipment = models.ForeignKey(Game, on_delete=models.CASCADE)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
